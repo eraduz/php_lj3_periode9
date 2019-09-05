c19a337020b891f542e70b27ed770b6075d01504
Reda <reda@eraduz.me>
2019-09-05 09:35:01 +0200

    finished opdracht 4

--- php_02/opdracht4.php
+++ php_02/opdracht4.php
@@ -6,27 +6,28 @@
  * Time: 11:13
 **/
 
-session_start();
 
-$conn = new mysqli('localhost', 'root', '', 'opdracht4');
-
-
-if(isset($_POST['username']) && isset($_POST['password'])){
+if(!empty($_POST['username']) && !empty($_POST['password'])){
+if(isset($_POST['username']) && isset($_POST['password']))
+    $conn = new mysqli('localhost', 'root', '', 'opdracht4');
     $username = $_POST['username'];
     $password = $_POST['password'];
-//    var_dump($_POST);
-    $query = "SELECT * FROM 'users' WHERE username='$username' AND WHERE password = '$password' ";
-    $result = mysqli_query($conn, $query);
-    var_dump($result);
-    $rows = mysqli_num_rows($result);
-    if($rows == 1){
-    $_SESSION['username'] = $username;
-    $_SESSION['password'] = $password;
-        echo "Goed";
-    }else{
+    $query = "SELECT * FROM `users` WHERE username = '$username' AND password = '$password' ";
+    $res = mysqli_query($conn, $query);
+    if(mysqli_num_rows($res) == 1){
+        session_start();
+        $_SESSION['username'] = $username;
+        $_SESSION['password'] =  $password;
+        while ($row = mysqli_fetch_assoc($res)) {
+            if($_SESSION['username'] == $row['username'] && $_SESSION['password'] == $row['password']){
+                echo 'Goed';
+            }
+        }
+    } else {
         echo 'Fout';
     }
 }
+
 ?>
 
 <form method="POST">
