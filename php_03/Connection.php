8a5053d6592cc537d40decabd93036b9391d525b
Reda <reda@eraduz.me>
2019-09-11 11:11:37 +0200

    added galgje (not done yet)

---
+++ php_03/Connection.php
@@ -0,0 +1,17 @@
+<?php
+class Connection
+{
+    private static $server = "mysql:host=localhost;dbname=galgje01";
+    private static $user = "root";
+    private static $password = "";
+    protected static $conn;
+    public static function openConnection()
+    {
+        try {
+            self::$conn = new PDO(self::$server,self::$user, self::$password);
+            return self::$conn;
+        } catch (PDOException $e) {
+            echo "There is some problem in connection: " . $e->getMessage();
+        }
+    }
+}
